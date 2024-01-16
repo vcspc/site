f7ea28bb2c55e904c7634e1ee2c9b33417f828b5
Vinicius <vitonilcius@gmail.com>
2024-01-16 14:53:12 -0300

    finalização do mecanismo de slide

--- site-vira/components/Slider2.jsx
+++ site-vira/components/Slider2.jsx
@@ -3,15 +3,18 @@ import styles from '../styles/Slider2.module.scss';
 
 function Slider2() {
   const [clipPath, setClipPath] = useState('50%');
+  const [clipPath2, setClipPath2] = useState('50%');
 
   const handleMouseMove = (event) => {
     // Calcular a posição do mouse como uma porcentagem do contêiner
     const containerWidth = event.currentTarget.offsetWidth;
     const mouseX = event.clientX - event.currentTarget.offsetLeft;
     const mousePercent = (mouseX / containerWidth) * 100;
-    
-    // Atualizar o estado do clip-path
+    const invertedPercent = 100 - mousePercent;
+
+    // Atualizar o estado dos clip-paths
     setClipPath(`${mousePercent}%`);
+    setClipPath2(`${invertedPercent}%`);
   };
 
   return (
@@ -19,13 +22,13 @@ function Slider2() {
       <img
         src="./vinicius_costa.png"
         className={styles.faceImage}
-        style={{ clipPath: `inset(0 ${clipPath} 0 0)` }}
+        style={{ clipPath: `inset(0 0 0 ${clipPath2})` }}
         alt="Face"
       />
       <img
         src="./victor_costa_completo.png"
         className={styles.faceImage2}
-        style={{ clipPath: `inset(0 0 0 ${clipPath})` }}
+        style={{ clipPath: `inset(0 ${clipPath} 0 0)` }}
         alt="Face"
       />
     </div>
