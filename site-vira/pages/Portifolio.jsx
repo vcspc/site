c64bdbcc1565507f536518aae98719be5e76817b
Vinicius <vitonilcius@gmail.com>
2024-01-24 15:02:40 -0300

    Atualização dos cards da página portifólio

--- site-vira/pages/Portifolio.jsx
+++ site-vira/pages/Portifolio.jsx
@@ -39,22 +39,22 @@ export default function Portifolio() {
                     descricao="Landing page desenvolvida para o projeto da marca Olivage"
                     />
                     <Card 
-                    src='./oliveage.png'
-                    alt='Imagem do projeto de landing page da marca Oliveage' 
-                    titulo="Landing page da marca Oliveage" 
-                    descricao="Landing page desenvolvida para o projeto da marca Olivage"
+                    src='./logo_heineken.png'
+                    alt='Imagem do logo da marca Heineken' 
+                    titulo="Projeto com a Heineken" 
+                    descricao="Lançamento de uma estação de coleta de long neck Heineken"
                     />
                     <Card 
-                    src='./livro.png' 
-                    alt="Imagem de um projeto de landing page de promoção do livro Nós Estivemos Aqui" 
-                    titulo="Landing page Nós Estivemos Aqui" 
-                    descricao="Landing page desenvolvida para o projeto de promoção do livro Nós Estivemos Aqui" 
+                    src='./amstel_logo.jpg' 
+                    alt="Imagem da marca Amstel" 
+                    titulo="Projeto com a Amstel" 
+                    descricao="Lançamento do De Dam - Snack Bar em Holambra" 
                     />
                     <Card 
-                    src="./bussola.png" 
-                    alt="Imagem de um projeto de landing page chamado Bússola do Escritor" 
-                    titulo="Landing page Bússola do Escritor" 
-                    descricao="Landing page desenvolvida para o projeto Bússola do Escritor" 
+                    src="./influenciadores.png" 
+                    alt="Imagem com vários influenciadores digitais" 
+                    titulo="Projetos com Influenciadores Digitais" 
+                    descricao="Trabalhos desenvolvidos com diferentes influenciadores no Instagram" 
                     />
                     <Card 
                     src="./bussola.png" 
