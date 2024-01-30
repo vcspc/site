f7655f5704a8d6d1672ac3465a73b99c15d32e47
vinicius costa <vitonilcius@gmail.com>
2024-01-29 23:51:22 -0300

    atualizado 3 cards da página portifólio

--- site-vira/pages/Portifolio.jsx
+++ site-vira/pages/Portifolio.jsx
@@ -57,22 +57,22 @@ export default function Portifolio() {
                     descricao="Trabalhos desenvolvidos com diferentes influenciadores no Instagram" 
                     />
                     <Card 
-                    src="./bussola.png" 
-                    alt="Imagem de um projeto de landing page chamado Bússola do Escritor" 
-                    titulo="Landing page Bússola do Escritor" 
-                    descricao="Landing page desenvolvida para o projeto Bússola do Escritor" 
+                    src="./clube_de_vantagens.png" 
+                    alt="Imagem de apresentação de um clube de vantagens" 
+                    titulo="Projeo de Clube de Vantagens" 
+                    descricao="Clube de Vantagens desenvolvido para o setor corporativo" 
                     />
                     <Card 
-                    src='./oliveage.png'
-                    alt='Imagem do projeto de landing page da marca Oliveage' 
-                    titulo="Landing page da marca Oliveage" 
-                    descricao="Landing page desenvolvida para o projeto da marca Olivage"
+                    src='./logo_martin_holandesa.png'
+                    alt='Imagem do logo da empresa Martin Holandesa' 
+                    titulo="Campanha de 67 anos da Martin Holandesa" 
+                    descricao="Criação da campanha de 67 anos da Martin Holandesa"
                     />
                     <Card 
-                    src="./bussola.png" 
-                    alt="Imagem de um projeto de landing page chamado Bússola do Escritor" 
-                    titulo="Landing page Bússola do Escritor" 
-                    descricao="Landing page desenvolvida para o projeto Bússola do Escritor" 
+                    src="./dia_das_maes.png" 
+                    alt="Imagem de mãe e filha se abraçando" 
+                    titulo="Campanha do Dia das Mães" 
+                    descricao="Campanha do dia das mães para aproximar o público femenino" 
                     />
 
                 </div>
