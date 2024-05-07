d63bd01c3bc45f124a26197df61823cc5a110611
vcspc <vitonilcius@gmail.com>
2024-05-07 15:38:09 -0300

    ajustado componente corpo para tela mobile

--- site-vira/components/Galeria.jsx
+++ site-vira/components/Galeria.jsx
@@ -1,17 +1,20 @@
 import styles from '../styles/Galeria.module.scss';
 
 export default function Galeria(props) {
+
+
     return (
         <div className={styles.galeria}>
-            {props.imagens.map((imagem, index) => (
-                <img key={index} 
-                     src={imagem} 
-                     alt={`Imagem ${index+1}`} 
-                     style={{width: props.largura || '20rem'}} 
-                     className={styles.galeria_imagem}/>
-            ))}
+                {props.imagens.map((imagem, index) => (
+                    <img key={index} 
+                        src={imagem} 
+                        alt={`Imagem ${index+1}`} 
+                        style={{width: props.largura || '20rem'}} 
+                        className={styles.galeria_imagem}/>
+                ))}
         </div>
+   
     );
+
+
 }
-
-
--- site-vira/pages/Teste.jsx
+++ site-vira/pages/Teste.jsx
@@ -4,11 +4,27 @@ import Layout from '../components/Layout';
 import MenuHamburguer from '../components/MenuHamburguer';
 import Galeria from '../components/Galeria';
 import Corpo from '../components/Corpo';
+import Apresentacao from '../components/Apresentacao';
+import useScreenWidth from '../components/UseScreenWidth';
 
 export default function Teste() {
+
+    const screenWidth = useScreenWidth();
+    const sentidoEsquerdo = screenWidth > 412 ? 'esquerda' : 'direita';
+    const sentidoDireito = screenWidth > 412 ? 'direita' : 'direita';
+    const sentidoLista = screenWidth > 412 ? 'lista' : 'listaDireita';
+
     return (
         <>
             <Layout>
+                <Apresentacao
+                    src="./vinicius_costa.png" 
+                    alt="Logo da empresa de marketing Vira"
+                    titulo="Vira"
+                    subtitulo="A empresa"
+                    descricao="Na Vira Marketing Digital, nós acreditamos no poder da tecnologia para transformar o cenário empresarial. Nosso nome, que significa 'mudar', 'transformar' ou 'inverter' em português, reflete nossa essência e missão: virar o jogo a favor de nossos clientes. Somos especialistas em criar estratégias de marketing digital inovadoras que não apenas captam a atenção do público, mas também convertem essa atenção em vendas, lucros e crescimento sustentável."
+                    sentido={sentidoDireito}
+                />
                 <Galeria 
                     imagens={[
                         './victor_costa_completo.png',
